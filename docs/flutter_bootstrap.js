b10cd486e89b06cd878f569303d9da561d3cbe46
Flav-legrand <mongouoflav@gmail.com>
2026-06-01 06:49:45 +0100

    Add web build to docs folder

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1859566723" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1387217629" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
