13676143cbc18488de51e00499114dfd39653c3d
Flav-legrand <mongouoflav@gmail.com>
2026-06-01 07:14:05 +0100

    Remplacer test1 par l'application Flutter Oyeo

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1201408741" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3621976427" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
